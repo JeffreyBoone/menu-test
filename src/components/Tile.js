c641b2ebe8a9258e33d74877dbdca588440aefc6
JeffreyBoone <302948965@student.rocmondriaan.nl>
2020-11-11 11:12:11 +0100

    1

---
+++ src/components/Tile.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  Container, Row, Col, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
+import {list} from "../props";
+import './menu.css';
+
+function goto() {
+
+}
+
+const Tile = () => {
+    return (  
+        <Container fluid={true}>
+          <Row>
+            {list.map((l) => 
+            <Col id={l.color}>
+             <CardBody id={l.color}>
+              <CardTitle tag="h5">{l.title}</CardTitle>
+              <CardSubtitle tag="h6" className="mb-2 text-muted">{l.desc}</CardSubtitle>
+            </CardBody><img id='img' src={l.image} alt="logo"/>
+            <CardBody>
+              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <CardLink onClick={goto()}></CardLink>
+            </CardBody>
+           </Col>)}
+          </Row>
+        </Container>
+      );
+}
+
+
+export default Tile;
--- src/props.js
+++ src/props.js
@@ -1,24 +1,14 @@
-export const title = {
-    handboek: 'Handboek - LIOR',
-    pve: "PvE Projecten",
-    bijlagen: 'Bijlagen',
-    BV: 'Bronverwijzingen',
-    AR: 'Accounts en Rechten',
-  };
-
-export const desc = {
-    handboek: 'Handboek Openbare Ruimte',
-    pve: "Programma van Eisen",
-    bijlagen: 'Raadplegen van Bijlagen',
-    BV: 'Raadplegen van Bronverwijzingen',
-    AR: 'Gebruikersadministratie',
-  };
-
+import image1 from '../src/1.png';
+import image2 from '../src/2.png';
+import image3 from '../src/3.png';
+import image4 from '../src/4.png';
+import image5 from '../src/5.png';
 
 export const list = [
-  {title: 'Handboek - LIOR', desc: 'Handboek Openbare Ruimte'},
-  {title: "PvE Projecten", desc: "Programma van Eisen"},
-  {title: 'Bijlagen', desc: 'Raadplegen van Bijlagen'},
-  {title: 'Bronverwijzingen', desc: 'Raadplegen van Bronverwijzingen'},
-  {title: 'Accounts en Rechten', desc: 'Gebruikersadministratie'}
+  {color: "c1", image: [image1], title: 'Handboek - LIOR', desc: 'Handboek Openbare Ruimte'},
+  {color: "c2", image: [image2], title: "PvE Projecten", desc: "Programma van Eisen"},
+  {color: "c2", image: [image3], title: 'Bijlagen', desc: 'Raadplegen van Bijlagen'},
+  {color: "c1", image: [image4], title: 'Bronverwijzingen', desc: 'Raadplegen van Bronverwijzingen'},
+  {color: "c1", image: [image5], title: 'Accounts en Rechten', desc: 'Gebruikersadministratie'},
+  {color: "c2", image: [image5], title: 'Extra', desc: 'Gebruikersadministratie'},
 ]

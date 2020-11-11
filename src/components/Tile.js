f4a007f4d0dfb2a636a7924ce84db65dc5cf1da9
JeffreyBoone <302948965@student.rocmondriaan.nl>
2020-11-11 11:48:59 +0100

    remove menu

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Container, Row, Col, CardText, CardBody, CardLink,
+  Container, Row, Col, CardText, CardBody,
   CardTitle, CardSubtitle
 } from 'reactstrap';
 import {list} from "../props";
@@ -15,14 +15,13 @@ const Tile = () => {
         <Container fluid={true}>
           <Row>
             {list.map((l) => 
-            <Col id={l.color}>
+            <Col id={l.color} onClick={goto()}>
              <CardBody id={l.color}>
               <CardTitle tag="h5">{l.title}</CardTitle>
               <CardSubtitle tag="h6" className="mb-2 text-muted">{l.desc}</CardSubtitle>
             </CardBody><img id='img' src={l.image} alt="logo"/>
             <CardBody>
               <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-            <CardLink onClick={goto()}></CardLink>
             </CardBody>
            </Col>)}
           </Row>
